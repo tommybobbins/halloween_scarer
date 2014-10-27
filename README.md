ce3f4806a2bcd444025cd84769697d043eab7ad0
Tim Gibbon <tng@chegwin.org>
2014-10-27 14:22:50 +0000

    Added image, first cut of code

---
+++ README.md
@@ -0,0 +1,26 @@
+Halloween Scarer
+====================
+
+![](IMG_20141027_140916.jpg)
+PIR sensor attached to pins 5V, GND and 7 (Board pin 26)
+LEDS attached to pins GND and 18 (Board pin 12)
+
+Needs Python Pygame to play the audio track:
+
+    sudo apt-get install python-pygame
+
+To run on boot:
+
+   chmod a+x /home/pi/halloween_scarer/light_zombie.py
+   sudo nano /etc/rc.local
+
+Just before the exit 0 line at the bottom, add:
+
+   /home/pi/halloween_scarer/light_zombie.py &
+
+Laugh taken from Freesound.org
+http://www.freesound.org/people/DocOne42/sounds/214630/
+CC BY-NC 3.0
+
+
+
---
+++ light_zombie.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+import pygame
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+from time import sleep
+pygame.init()
+currState = False
+prevState = False
+
+while True:
+    try:
+        sleep(0.1)
+        prevState = currState
+        currState = GPIO.input(7)
+        if currState != prevState:
+            newState = "HIGH" if currState else "LOW"
+            print "GPIO pin %s is %s" % (7, newState)
+            GPIO.output(18, True)
+            sleep(1)
+            pygame.mixer.music.load("laugh.mp3")
+            pygame.mixer.music.play()
+            GPIO.output(18, False)
+            sleep(1)
+            GPIO.output(18, True)
+            sleep(5)
+            GPIO.output(18, False)
+            sleep(5)
+            prevState = GPIO.input(7)
+    except (KeyboardInterrupt, SystemExit):
+        GPIO.cleanup()
+        print ("Keyboard stop")
+        exit()
+    except:
+        # report error and proceed
+        GPIO.cleanup()
+        print ("FUBAR")
+        exit()
+
+
